f7c08cab95e922f439ea65ae163a6cf4ca3117d6
Keith Calise <KeithCalise@gmail.com>
2018-10-20 20:56:37 -0400

    tweaking game flow

--- mystery_word.py
+++ mystery_word.py
@@ -58,6 +58,7 @@ def user_Input():
     modage = mode.upper()
 
     return modage
+    
 
 
 
@@ -67,16 +68,31 @@ def get_word(modage, words):
         easy_list = easy_words(words)
         word1 = random_word(easy_list).upper()
         print("Your word has {} letters in it".format(len(word1)))
+        word_cart = []
+        for i in range(len(word1)):
+            word_cart.append("{}".format("_"))
+        word_show = " ".join(word_cart)
+        print word_show
         return word1
     elif modage == 'M'.upper():
         medium_list = medium_words(words)
         word2 = random_word(medium_list).upper()
         print("Your word has {} letters in it".format(len(word2)))
+        word_cart = []
+        for i in range(len(word2)):
+            word_cart.append("{}".format("_"))
+        word_show = " ".join(word_cart)
+        print word_show
         return word2
     elif modage == 'H'.upper():
         hard_list = hard_words(words)
         word3 = random_word(hard_list).upper()
         print("Your word has {} letters in it".format(len(word3)))
+        word_cart = []
+        for i in range(len(word3)):
+            word_cart.append("{}".format("_"))
+        word_show = " ".join(word_cart)
+        print word_show
         return word3
 
         #need to put a catch all here
@@ -84,28 +100,40 @@ def guess_letters(any_word):
     count = 8
     guessed_letter=[]
     missed_letter = []
+    print guessed_letter
     while count != 0:
         guess1 = raw_input("Please guess a letter: ").upper()
-        guess1 = re.sub(r'[^A-Za-z0-9]','',guess1)
-        if guess1 in guessed_letter:
-            print('You guessed that already but,')
+        guess1 = re.sub(r'[^A-Za-z0-9]','',guess1)        
+        if is_word_complete(any_word, guessed_letter)==True:
+              print("Congratulations, you have solved the mystery word, it was {}.".format(any_word))
+              break
+        elif guess1 in guessed_letter or guess1 in missed_letter:
+            print('You guessed that already but we wont hold that against you')
             guessed_letter.append(guess1)
         elif len(guess1) > 1:
             print("That is invalid, please try again.")
-        elif is_word_complete(any_word, guessed_letter)==True:
-                print("Congratulations, you have solved the mystery word, it was {}.".format(any_word))
-                break
+        
         elif guess1 in any_word:
             guessed_letter.append(guess1)
             print("Good guess!!")
             print(display_word(any_word, guessed_letter))
-            print("You have {} guesses left".format(count))
+            if count > 1:
+                print("You still have {} guesses left".format(count))
+            else:
+                print("You still have {} guess left".format(count))
+
         elif guess1 not in any_word:
             missed_letter.append(guess1)
+            guessed_letter.append(guess1)
             print('Sorry that letter is not in the word')
+            print(display_word(any_word, guessed_letter))
             if guess1 in missed_letter:
                 count-=1
-            print("You have {} guesses left".format(count))
+            if count > 1:
+                print("You have {} guesses left".format(count))
+            else:
+                print("You have {} guess left".format(count))
+
             if count == 0:
                 print("Sorry, you lose. The word was {}".format(any_word))
     return
